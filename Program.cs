4d20ef57d138b5ff0efba5f96d00d2a766b2265b
srlopezh <srlopezh@gmail.com>
2021-09-13 12:03:09 +0200

    cometarios

--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
 ﻿/*
+PROGRAMACION IMPERATIVA
 con las siguientes instrucciones IF, GOTO y LABEL
 calcula la media del array
-- todo variables globales
-- ninguna función
-*/
 
+Restricciones
+    - todo variables globales
+    - ninguna función
+    - no usar llaves {} para bloques de instruciones
+    - if sin else.
+*/
     using System;
 
-    var notas = new []{ 7.1M, 4, 6, 5, 4, 6.5M, 7 };
+    var notas = new []{ 7.5M, 4, 6, 5, 4, 6.5M, 7.5M };
+    //var notas = new []{ 1M,1,1,1,1,1 };
     var suma = 0M;
     var media = 0M;
     var index = 0;
 
+//  EMPEZAMOS
     Console.WriteLine("Empiezo");
+
+//  CICLO
 sumar:
     suma = suma + notas[index];
     index = index +1;
     if(index<notas.Length) goto sumar;
 
+//  FIN CICLO
     Console.WriteLine("fin ciclo");
     media = suma / notas.Length;
 
-    Console.WriteLine($"La media es {media}");
+//  FINAL
+    Console.WriteLine($"La media es {media:0.00}");
