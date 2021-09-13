3b492cc957fdc06187611528b7d74841bf0f0929
srlopez <you@example.com>
2021-09-13 15:55:08 +0200

    master

--- Program.cs
+++ Program.cs
@@ -17,104 +17,67 @@ y Dictionary<string, object> https://docs.microsoft.com/es-es/dotnet/api/system.
 */
 
 using System;
-using System.Collections.Generic;
-
 
 Console.WriteLine("Empezamos");
-//var notas = new[] { 7.5M, 4, 6, 5, 4, 6.5M, 7.5M };
 
-// Tuplas
-var tLuis = (Nombre: "Luis", Nota: 7.5M);
-var tMarta = (Nombre: "Marta", Nota: 4M);
-var tMarcos = ("Marcos", 6M);  //<--- Ojo! sin nombre
-var tNotas = new[] { tLuis, tMarta, tMarcos };
-Tuple<string, int>[] tNotas0 = new Tuple<string, int>[] { }; //<-- inicializado a 0 elementos
-Console.WriteLine($"La primera nota del array de tuplas es {tNotas[0].Item1:0.00}");
+Calificacion[] notas = new[] {
+        //Luis, Marta, Marcos, Aroa, Nerea, Kike, Juan
+        //7.5M, 4,     6,      5,    4,     6.5M, 7.5M 
+        new Calificacion("Luis", 7.5M),
+        new Calificacion("Marta", 4),
+        new Calificacion("Marcos", 6),
+        new Calificacion("Aroa", 5),
+        new Calificacion("Nerea", 4),
+        new Calificacion("Kike", 6.5M),
+        new Calificacion("Juan", 7.5M)
+    };
 
-// Estructuras
-var sLuis = new SCalificacion("Luis", 7.5M);
-var sNotas = new[] { sLuis, /*...*/};
-SCalificacion[] sNotas0 = new SCalificacion[] { };//<-- inicializado a 0 elementos
-Console.WriteLine($"La primera nota del array de estructuras es {sNotas[0].Nota:0.00}");
-
-
-// Records
-var rluis = new ReCalificacion("Luis", 7.5M);
-var rNotas = new[] { rluis, /*..*/};
-ReCalificacion[] rNotas0 = new ReCalificacion[8]; //<- inicializado longitud 8 y cero elementos
-Console.WriteLine($"La primera nota del array de records es {rNotas[0].Nota:0.00}");
-
-// Clases
-var cLuis = new ClCalificacion("Luis", 7.5M);
-var cNotas = new[] { cLuis, /*...*/};
-ClCalificacion[] cNotas0 = new ClCalificacion[] { };//<-- inicializado a 0 elementos
-Console.WriteLine($"La primera nota del array de clases es {cNotas[0].Nota:0.00}");
-
-// Diccionario
-var dLuis =  new Dictionary<string, Object>();
-dLuis["nombre"] = "Luis";
-dLuis["nota"] = 7.5M;
-var dNotas = new[] { dLuis, /*...*/};
-var dNotas0 = new Dictionary<string, Object>[7] ;
-Console.WriteLine($"La primera nota del array de diccionarios es {dNotas[0]["nota"]:0.00}");
+var Sistema = new Sistema(notas);
+Console.WriteLine($"La media de la notas es: {Sistema.CalculoDeLaMedia():0.00}");
+Console.WriteLine("Fin");
 
 
-// TopLevel instrucciones obliga a las declaraciones abajo
-public struct SCalificacion
-{
-    public SCalificacion(string nombre, decimal nota)
-    {
-        Nombre = nombre;
-        Nota = nota;
-    }
-
-    public string Nombre { get; }
-    public decimal Nota { get; }
-
-    public override string ToString() => $"({Nombre}, {Nota})";
-}
-
-// Registros
-public record ReCalificacion(string Nombre, decimal Nota);
-
-public class ClCalificacion
+public class Calificacion
 {
     public string Nombre;
     public decimal Nota;
-    public ClCalificacion(string nombre, decimal nota)
+    public Calificacion(string nombre, decimal nota)
     {
         Nombre = nombre;
         Nota = nota;
     }
     public override string ToString() => $"({Nombre}, {Nota})";
 }
-/*
-Elige la opción más adecuada según tu criterio
-para representar la Calificación
-*/
 
-
-/*
-var suma = CalculoDeLaSuma(notas);
-var media = CalculoDeLaMedia(suma, notas.Length);
-Console.WriteLine($"La media es {media:0.00}");
-Console.WriteLine("Fin");
-
-decimal CalculoDeLaSuma(decimal[] datos)
+public class Sistema
 {
-    var suma = 0M;
-    var index = 0;
-    do
-    {
-        suma += datos[index];
-        index++;
+
+    Calificacion[] Notas;
+
+    public Sistema( Calificacion[] notas ){
+        Notas = notas;
     }
-    while (index < datos.Length);
-    return suma;
+
+    private decimal CalculoDeLaSuma(decimal[] datos)
+    {
+        var suma = 0M;
+        foreach(decimal val in datos)
+        {
+            suma += val;
+        }
+        return suma;
+    }
+
+    public decimal CalculoDeLaMedia()
+    {
+
+        var notas = new decimal[Notas.Length];
+        var i = 0;
+        foreach (Calificacion cal in Notas)
+        {
+            notas[i++] = cal.Nota;
+        }
+        return CalculoDeLaSuma(notas) / Notas.Length;
+    }
 }
-
-decimal CalculoDeLaMedia(decimal total, int nElementos) => total / nElementos;
-
-
- */
 
